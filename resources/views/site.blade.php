4db889b2c4060b9451b9230f497f2f2c7ce02a5b
andririzkisaputra <andri.rizki007@gmail.com>
2023-09-14 09:14:38 +0700

    first commit

---
+++ resources/views/site.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sovet</title>
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ url('css/custom.css') }}" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container col-12 mb-5">
+        <nav class="mt-5 row">
+            <div class="col-6">
+                <h1>
+                    <a class="navbar-brand" href="{{ route('site') }}">SOVET</a>
+                    <span class="header-tm">TM</span>
+                </h1>
+            </div>
+            <div class="col-6 text-right">
+                <h4>Event Organizer</h4>
+            </div>
+        </nav>
+        <div class="slider">
+            <p class="header-text">Company</p>
+            <p>Profile</p>
+        </div>
+        <div class="about">
+            <p class="title">About Company</p>
+            <i style="font-weight: bold; font-size: 20px;">
+                "We are a company that specialized on bellow the line activation"
+            </i>
+            <br>
+            <span>
+                In this enviroment of fast information and various option that flooded
+                the market, brands need to adapt a new and effective communication
+                compaign. A successful bellow the line activation has to be unique
+                and one experience that is marked on the consumer mind. Only bellow the
+                line activation can reach the consumer in avery close and personal engagement,
+                and it is proven effective. Personal Brand EXPERENCE is all that it take to win
+                your consumers hearts in a most exciting and excellent way. We are customized for
+                personalize services for every activation we engage, because we believe that
+                each brand own it's own it's own uniqueness and image to be srengthened. We
+                never generalize our services for our client. At amother point, web activate
+                our clients campaigns with our hearts, that's what makes us than outhers.
+            </span>
+        </div>
+        <div class="our-services pt-5 mt-5">
+            <p class="title">OUR SERVICES</p>
+            <div class="grid-container">
+                <span class="grid-item">Crative Coception</span>
+                <span class="grid-item">Design and Executin</span>
+                <span class="grid-item">Event Management</span>
+                <span class="grid-item">Brand Activation</span>
+                <span class="grid-item">Launching Event</span>
+                <span class="grid-item">Company MICE</span>
+                <span class="grid-item">Press Conference</span>
+                <span class="grid-item">Meeting Conference</span>
+            </div>
+        </div>
+        <div class="our-event pt-5 mt-5">
+            <p class="title">OUR EVENT</p>
+            <div class="grid-container">
+                <span class="grid-item">Crative Coception</span>
+                <span class="grid-item">Design and Executin</span>
+                <span class="grid-item">Event Management</span>
+                <span class="grid-item">Brand Activation</span>
+                <span class="grid-item">Launching Event</span>
+                <span class="grid-item">Company MICE</span>
+                <span class="grid-item">Press Conference</span>
+                <span class="grid-item">Meeting Conference</span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
