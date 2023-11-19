e025565db5aae6456cf3c6621c89d1a8ba52982f
andririzkisaputra <andri.rizki007@gmail.com>
2023-11-20 00:58:07 +0700

    update GOOGLE-SITE-VERIFICATION

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title>@yield('title') | {{ ENV('APP_NAME') }}</title>
+    <meta name="google-site-verification" content="{{ ENV('GOOGLE-SITE-VERIFICATION') }}">
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="@yield('title') | {{ ENV('APP_NAME') }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
